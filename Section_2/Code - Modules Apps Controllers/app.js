67ed36f388d55011c9350f0cb6d26a221f883975
Sounak Saha <sounaksaha999@gamil.com>
2023-11-01 13:41:22 +0530

    Section 3 updated

--- Section_2/Code - Modules Apps Controllers/app.js
+++ Section_2/Code - Modules Apps Controllers/app.js
@@ -3,7 +3,14 @@ var angularApp = angular.module('angularApp', []);
 
 // CONTROLLERS
 angularApp.controller('mainController', ['$scope', function ($scope) {
-    
+    $scope.name = 'John Doe'
+    $scope.occupation = 'Coder'
+
+    $scope.getname = function () {
+        return 'John Doe'
+    }
+
+    console.log($scope);
 }]);
 
 
